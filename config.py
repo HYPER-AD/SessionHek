73aeddce370f5124a8813efa0c760651a2d73dcf
ʜʏᴘᴇʀ <72803194+HYPER-AD@users.noreply.github.com>
2023-03-30 11:15:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,23 +1,7 @@
-from os import getenv
-
-from dotenv import load_dotenv
-
-load_dotenv()
-
-
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
-
-TOKEN = getenv("TOKEN", None)
-TOKEN = getenv("START_PIC", None)
-
-
-
-"""
 class Config:
-    API_ID = 
-    API_HASH = ""
-    TOKEN = ""  
-    START_PIC = "" 
-    CHAT = ""    
-"""
+    API_ID = 18551492
+    API_HASH = "9046713fdc162e06212f23e0e2047b4b"
+    TOKEN = "6235942365:AAFxh9UvAgXFx0batWr6QUrrCBNdMg6uLPM"  
+    START_PIC = "https://te.legra.ph/file/f105222f8dbeb1ebe8e4d.jpg" 
+    CHAT = "@telegram"    
+
