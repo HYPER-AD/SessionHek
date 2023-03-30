e82e968dde196b6ffc5c600fe68ea6b4c6daa340
ʜʏᴘᴇʀ <72803194+HYPER-AD@users.noreply.github.com>
2023-03-30 11:10:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,23 @@
+from os import getenv
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+API_ID = int(getenv("API_ID"))
+API_HASH = getenv("API_HASH")
+
+TOKEN = getenv("TOKEN", None)
+TOKEN = getenv("START_PIC", None)
+
+
+
+"""
 class Config:
     API_ID = 
     API_HASH = ""
     TOKEN = ""  
     START_PIC = "" 
     CHAT = ""    
+"""
